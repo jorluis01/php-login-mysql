74612a47661b168d9c4fbfcb2e20ef6dd61c4b09
Matius <jorluis.01@gmail.com>
2015-10-06 12:25:52 -0500

    version  0.01

---
+++ modelo/conexion.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of conexion
+ *
+ * @author DR ROBERTO
+ */
+class conexion {
+    private $url;
+    private $contra;
+    private $user;
+    private $host;
+    public function __construct() {
+        
+    }
+    
+    
+    
+}
